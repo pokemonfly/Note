2e14a179c9bc533913bf7489cf508e56de6b63b5
pokemonfly <pokemonfly@gmail.com>
2015-03-31 13:42:41 +0800

    1

--- node.js
+++ node.js
@@ -1,6 +1,29 @@
 var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
 var app = express();
-app.configure(function() {
-  app.use(express.bodyParser());
+
+// all environments
+app.set('port', process.env.PORT || 80);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-app.listen(80);
